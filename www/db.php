8ee1569079b8b6d2936ea794ce7d249a91529560
tumi <happyzhangyuting@163.com>
2018-11-22 01:30:49 +0800

    init

--- www/db.php
+++ www/db.php
@@ -1,8 +1,9 @@
 <?php
 
-$dsn = 'mysql:host=mysql;dbname=app';
-$username = 'app';
-$passwd = '123456';
+//var_dump(1);
+$dsn = 'mysql:host=back-network;dbname=docker_test';
+$username = 'tumi';
+$passwd = 'tumi';
 $dbh = new PDO($dsn, $username, $passwd);
 
 var_dump($dbh);
